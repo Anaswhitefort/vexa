16482e843fdd3128519abe118ad5c64f9271b4b6
ANAS <30664656+Anaswhitefort@users.noreply.github.com>
2023-06-20 21:44:04 +0400

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -8,10 +8,9 @@ const Banner = (props) => (
             </header>
             <div className="content">
                 <p>Partner with VEXA and experience the power of digital marketing and video production done right. 
-Let us help you elevate your brand, engage your audience, and achieve remarkable success in the digital landscape. 
-Get in touch with us today to discuss your goals and take the first step towards digital excellence.</p>
+Let us help you elevate your brand, engage your audience, and achieve remarkable success in the digital landscape.</p>
                 <ul className="actions">
-                    <li><a href="#one" className="button next scrolly">Get Started</a></li>
+                    <li><a href="tel:+971563901109" className="button next scrolly">Call Now</a></li>
                 </ul>
             </div>
         </div>
