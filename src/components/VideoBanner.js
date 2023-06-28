f0a725d0921b26933439efb7917a9d7c6f674161
ANAS <30664656+Anaswhitefort@users.noreply.github.com>
2023-06-28 16:28:37 +0400

    Update VideoBanner.js

--- src/components/VideoBanner.js
+++ src/components/VideoBanner.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import vexa from '..src/assets/images/vexa.mp4'
+import vexa from '../assets/images/vexa.mp4'
 const VideoBanner = () => {
   return (
     <div className="video-banner">
       <video autoPlay loop muted playsInline>
-        <source src="src/assets/images/vexa.mp4" type="video/mp4" />
+        <source src="/assets/images/vexa.mp4" type="video/mp4" />
       </video>
     </div>
   );
