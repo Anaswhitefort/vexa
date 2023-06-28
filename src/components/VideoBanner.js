bd814c32d0fbb675d7a5926030ed2326d9bbb4af
ANAS <30664656+Anaswhitefort@users.noreply.github.com>
2023-06-29 01:02:58 +0400

    Update VideoBanner.js

--- src/components/VideoBanner.js
+++ src/components/VideoBanner.js
@@ -3,7 +3,7 @@ const VideoBanner = () => {
   return (
     <div className="video-banner">
       <video autoPlay loop muted playsInline>
-        <source src="https://cdn.shopify.com/videos/c/o/v/3caec39258da4ef6ab4808232d8e4779.mp4" type="video/mp4" />
+        <source src="https://cdn.shopify.com/videos/c/o/v/a8d5b182b62f4505974c436c2efa37f6.mp4" type="video/mp4" />
       </video>
     </div>
   );
