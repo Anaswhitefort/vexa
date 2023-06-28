8b72a54a5841f81823ac2dc3e17f3e5b472b81bf
ANAS <30664656+Anaswhitefort@users.noreply.github.com>
2023-06-28 16:35:19 +0400

    Update VideoBanner.js

--- src/components/VideoBanner.js
+++ src/components/VideoBanner.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import vexa from 'https://cdn.shopify.com/videos/c/o/v/acf94bca91254cecbe62f9f64e8383ba.mp4';
 const VideoBanner = () => {
   return (
     <div className="video-banner">
