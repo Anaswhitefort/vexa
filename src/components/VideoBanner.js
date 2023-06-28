4b107f7c709a1e5a83c15206c51ca70690012f65
ANAS <30664656+Anaswhitefort@users.noreply.github.com>
2023-06-28 23:55:27 +0400

    Update VideoBanner.js

--- src/components/VideoBanner.js
+++ src/components/VideoBanner.js
@@ -1,5 +1,18 @@
 import React from 'react';
+
 const VideoBanner = () => {
+  const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
+  if (isMobile) {
+    return (
+      <div className="video-banner">
+        <video autoPlay loop muted playsInline>
+          <source src="https://cdn.shopify.com/videos/c/o/v/7f7a08e0871b4d0d89bf41a032ce4702.mp4" type="video/mp4" />
+        </video>
+      </div>
+    );
+  }
+
   return (
     <div className="video-banner">
       <video autoPlay loop muted playsInline>
