2b779d8dd751e334a1d6ddc3bb28db51fcb2eadf
ANAS <30664656+Anaswhitefort@users.noreply.github.com>
2023-07-23 09:17:35 +0400

    Update Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,9 +1,66 @@
 import React from 'react'
+import axios from "axios";
+import { Link } from "gatsby"
 
-const Contact = (props) => (
+
+const Contact = (props) => {
+
+  const [serverState, setServerState] = useState({
+      submitting: false,
+      status: null
+    });
+    const handleServerResponse = (ok, msg, form) => {
+      setServerState({
+        submitting: false,
+        status: { ok, msg }
+      });
+      if (ok) {
+        form.reset();
+      }
+    };
+    const handleOnSubmit = e => {
+      e.preventDefault();
+      const form = e.target;
+      setServerState({ submitting: true });
+      axios({
+        method: "post",
+        url: "https://getform.io/f/881e2908-a5d7-4760-baa9-0348a2987c6c",
+        data: new FormData(form)
+      })
+        .then(r => {
+          handleServerResponse(true, "Thanks!", form);
+        })
+        .catch(r => {
+          handleServerResponse(false, r.response.data.error, form);
+        });
+
+   };
+
+
+
+return (
     <section id="contact">
         <div className="inner">
-           
+            <section>
+                <form onSubmit={handleOnSubmit}>
+                    <div className="field half first">
+                        <label htmlFor="name">Name</label>
+                        <input type="text" name="name" id="name" />
+                    </div>
+                    <div className="field half">
+                        <label htmlFor="email">Email</label>
+                        <input type="text" name="email" id="email" />
+                    </div>
+                    <div className="field">
+                        <label htmlFor="message">Message</label>
+                        <textarea name="message" id="message" rows="6"></textarea>
+                    </div>
+                    <ul className="actions">
+                        <li><input type="submit" value="Send Message" className="special" /></li>
+                        <li><input type="reset" value="Clear" /></li>
+                    </ul>
+                </form>
+            </section>
             <section className="split">
                 <section>
                     <div className="contact-method">
